946fef93824f7435f70490760d58a8876de645f2
elvisshengjie <elvisshengjie.lim@digipen.edu>
2025-09-12 07:50:43 +0800

    Window.cpp
    Window application

---
+++ build/CMakeFiles/fc-tmp/stb/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing update step for stb")
+
+block(SCOPE_FOR VARIABLES)
+
+include("C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/CMakeFiles/fc-tmp/stb/stb-gitupdate.cmake")
+
+endblock()
---
+++ build/_deps/stb-build/stb_image_impl.cpp
@@ -0,0 +1,3 @@
+// Auto-generated at configure time
+#define STB_IMAGE_IMPLEMENTATION
+#include "C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/_deps/stb-src/stb_image.h"
