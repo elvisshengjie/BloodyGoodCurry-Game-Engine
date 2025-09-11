0d19fe4a709e543c982d0524d5418d1d5fddf119
elvisshengjie <elvisshengjie.lim@digipen.edu>
2025-09-12 07:52:05 +0800

    clean

--- build/CMakeFiles/fc-tmp/glfw/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for glfw")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/CMakeFiles/fc-tmp/glfw/glfw-gitupdate.cmake")
-
-endblock()
--- build/CMakeFiles/fc-tmp/imgui/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for imgui")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/CMakeFiles/fc-tmp/imgui/imgui-gitupdate.cmake")
-
-endblock()
