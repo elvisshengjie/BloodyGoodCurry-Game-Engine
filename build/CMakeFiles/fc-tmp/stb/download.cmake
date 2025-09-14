6ab1162214a7a3c9d14664732b85f9eb55eefdd1
elvisshengjie <elvisshengjie.lim@digipen.edu>
2025-09-14 22:19:53 +0800

    .

--- build/CMakeFiles/fc-tmp/stb/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for stb")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/CMakeFiles/fc-tmp/stb/stb-gitclone.cmake")
-
-endblock()
--- build/_deps/stb-build/stb_image_impl.cpp
+++
@@ -1,3 +0,0 @@
-// Auto-generated at configure time
-#define STB_IMAGE_IMPLEMENTATION
-#include "C:/Users/elvis/Documents/csd2401f25_team_sofasqud/build/_deps/stb-src/stb_image.h"
