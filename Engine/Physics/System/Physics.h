b90b0ff1fb3aa6fffa3d3cb4ed75f0fb16838b98
gbhosl <gbhosl@gmail.com>
2025-09-22 19:35:15 +0800

    Basic Physics implementation

--- Engine/Physics/System/Physics.h
+++ Engine/Physics/System/Physics.h
@@ -1,2 +1,32 @@
 #pragma once
 // Physics.h
+#include <vector>
+#include "Physics/Collision/Collision.h"
+
+namespace Framework
+{
+	class PhysicsObject
+	{
+	public:
+		PhysicsObject(float x, float y, float width, float height);
+
+		void SetVelocity(float vx, float vy);
+		void Update(float dt);
+
+		AABB GetAABB() const;
+
+		float posX, posY;
+		float width, height;
+		float velX = 0.0f, velY = 0.0f;
+	};
+
+	class PhysicsSystem
+	{
+	public:
+		void addObject(PhysicsObject* obj);
+		void Update(float dt);
+
+	private:
+		std::vector<PhysicsObject*> objects;
+	};
+}
