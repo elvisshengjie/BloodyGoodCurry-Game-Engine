e4431f9a8cb1dbaf8a7785315cf34dd1f0f80970
Anewbieprogram3r <83967493+Anewbieprogram3r@users.noreply.github.com>
2025-09-25 16:48:35 +0800

    Added stdfileSystem

--- Engine/Resource_Manager/Resource_Manager.h
+++ Engine/Resource_Manager/Resource_Manager.h
@@ -3,17 +3,18 @@
 #include "../Audio/SoundManager.h"
 #include "../Graphics/Graphics.hpp"
 #include <string>
+#include <filesystem>
 #include <algorithm>
 #include <iostream>
 class Resource_Manager
 {
 public:
-    enum Resource_Type { Graphics, Sound, All };
+    enum Resource_Type { Texture, Font, Graphics, Sound, All };
     struct Resources { unsigned int id; Resource_Type type; };
     static bool load(const std::string& name, const std::string& path, bool loop = false);
     static void unloadAll(Resource_Type type); 
     static inline std::unordered_map<std::string, Resources> resources_map;
-
+    
     // Helper Functions
     static inline std::string GetExtentsion(const std::string& path);
     static bool isGraphics(const std::string& ext);
