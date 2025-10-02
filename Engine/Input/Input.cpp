98ce9b6bedeeb2df770c21dbb98cd8003429a228
gbhosl <gbhosl@gmail.com>
2025-10-02 09:46:01 +0800

    Revert "Implemented input tests for keyboard and mouse"
    This reverts commit f4f01a868e81e1fd67901461e1446436064b1536.

--- Engine/Input/Input.cpp
+++ Engine/Input/Input.cpp
@@ -28,8 +28,7 @@ namespace Framework
 		// Initialize keys of interest
 		int keys[] = {
 			GLFW_KEY_W, GLFW_KEY_A, GLFW_KEY_S, GLFW_KEY_D,
-			GLFW_KEY_UP, GLFW_KEY_DOWN, GLFW_KEY_LEFT, GLFW_KEY_RIGHT,
-			GLFW_KEY_SPACE
+			GLFW_KEY_UP, GLFW_KEY_DOWN, GLFW_KEY_LEFT, GLFW_KEY_RIGHT
 		};
 
 		for (int key : keys)
--- Sandbox/MyGame/Game.cpp
+++ Sandbox/MyGame/Game.cpp
@@ -25,7 +25,6 @@
 #include <Graphics/Graphics.hpp>
 #include <Serialization/JsonSerialization.h>
 #include "Factory/Factory.h"
-#include "Input/Input.h"
 
 #include "Component/TransformComponent.h"
 #include "Component/RenderComponent.h"
@@ -70,7 +69,6 @@ namespace mygame
     static unsigned int gPlayerTex = 0;
 
     Framework::GOC* sRectObj = nullptr;
-    Framework::InputManager gInput(nullptr);
     static std::vector<Framework::GOC*> sLevelObjs;
     // ------------------------------------------------------------
     // Init: called once by Core, receives the created Window
@@ -79,7 +77,7 @@ namespace mygame
     {
         gWin = &win;
         using namespace Framework;
-        gInput = Framework::InputManager(gWin->raw());
+
         // 1) Create the factory (sets FACTORY)
         sFactory = std::make_unique<GameObjectFactory>();
 
@@ -205,7 +203,6 @@ namespace mygame
         AABB ahitbox(0, 0, 0, 0);
         AABB bhitbox(0, 0, 0, 0);
         handleAudioInput(*gWin, gKeyEdge, busInstance);
-        gInput.Update();
 
         // sweep factory once per frame (handles deferred destroys)
         if (sFactory) sFactory->Update(dt);
@@ -260,23 +257,6 @@ namespace mygame
                 if (gWin->isKeyPressed(GLFW_KEY_S)) tr->y -= rbc->velY * dt;
             }
         }
-
-        // Test Keyboard inputs
-        if (gInput.IsKeyPressed(GLFW_KEY_SPACE))
-            std::cout << "Spacebar pressed!" << std::endl;
-        if (gInput.IsKeyHeld(GLFW_KEY_SPACE))
-            std::cout << "Spacebar held!" << std::endl;
-        if (gInput.IsKeyReleased(GLFW_KEY_SPACE))
-            std::cout << "Spacebar released!" << std::endl;
-
-        // Test mouse inputs
-        if (gInput.IsMousePressed(GLFW_MOUSE_BUTTON_LEFT))
-            std::cout << "LMB pressed!" << std::endl;
-        if (gInput.IsMouseHeld(GLFW_MOUSE_BUTTON_LEFT))
-            std::cout << "LMB held!" << std::endl;
-        if (gInput.IsMouseReleased(GLFW_MOUSE_BUTTON_LEFT))
-            std::cout << "LMB released!" << std::endl;
-
         for (auto* obj2 : sLevelObjs) {
             if (obj2->GetObjectName() == "Player") {
                 sTestObj = obj2;
