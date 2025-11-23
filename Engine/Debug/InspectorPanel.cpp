fe9ff830e87800b759433d0d81c1c551ab541157
elvisshengjie <elvisshengjie.lim@digipen.edu>
2025-11-23 11:37:52 +0800

    Alt tab and cltr alt delete
    if alt tab or cltr dlt delete audio paused all keyboard transition cleared, if false, audio resume and input clear again to avoid stray event

--- Engine/Debug/InspectorPanel.cpp
+++ Engine/Debug/InspectorPanel.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************************************
  \file      InspectorPanel.cpp
  \par       SofaSpuds
- \author    
+ \author     elvisshengjie.lim ( elvisshengjie.lim@digipen.edu) - Primary Author, 100%
  \brief     Implements a basic ImGui inspector window for viewing and editing the currently
             selected GameObject's properties and common component data.
 *********************************************************************************************/
