4f44ece7543637e92f6b55a2a052addb93cdc332
Anewbieprogram3r <83967493+Anewbieprogram3r@users.noreply.github.com>
2025-11-25 18:30:37 +0800

    Test on new audio

--- Engine/Component/AudioComponent.h
+++ Engine/Component/AudioComponent.h
@@ -21,19 +21,31 @@ namespace Framework
         std::unordered_map<std::string, SoundInfo> sounds;
         std::unordered_map<std::string, bool> playing;
         float volume{ 1.0f }; 
-        
-        
-        AudioComponent()
-        {
-            sounds["footsteps"] = { "", false };
-            sounds["Slash1"] = { "", false };
-            sounds["GrappleShoot1"] = { "", false };
-            playing["footsteps"] = false;
-            playing["Slash1"] = false;
-            playing["GrappleShoot1"] = false;
-        };
+        std::string entityType;
 
-        void initialize() override {}
+        AudioComponent() = default;
+        
+        void initialize() override
+        {
+            // If sounds are empty, auto-populate based on entity type
+            if (sounds.empty())
+            {
+                if (entityType == "player")
+                {
+                    sounds["footsteps"] = { "footsteps.wav", true };
+                    sounds["Slash1"] = { "Slash1.wav", false };
+                    sounds["GrappleShoot1"] = { "GrappleShoot1.wav", false };
+                }
+                else if (entityType == "enemy")
+                {
+                    sounds["GhostSounds"] = { "GhostSounds.wav", false };
+                }
+
+                // Initialize playing map
+                for (auto& [action, info] : sounds)
+                    playing[action] = false;
+            }
+        }
     
         void Play(const std::string& action)
         {
